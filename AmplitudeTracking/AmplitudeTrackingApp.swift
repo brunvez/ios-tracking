5be035a670ab532bbefdeff13439162a7563024c
Bruno Vezoli <bruno.vezoli@circlemedical.com>
2023-05-27 15:17:32 +0200

    Project init

---
+++ AmplitudeTracking/AmplitudeTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmplitudeTrackingApp.swift
+//  AmplitudeTracking
+//
+//  Created by Bruno Vezoli on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AmplitudeTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
